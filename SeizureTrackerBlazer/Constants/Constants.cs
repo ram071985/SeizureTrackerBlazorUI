875ebdf6f81a0262e93e60c702f1522241997a9b
ram071985 <reidmuchow@gmail.com>
2024-12-30 11:02:57 -0600

    adds constants and more form inputs. figuring out multi select.

---
+++ SeizureTrackerBlazer/Constants/Constants.cs
@@ -0,0 +1,21 @@
+namespace SeizureTrackerBlazer.Constants
+{
+    internal static class UIConstants
+    {
+        internal const string MultiSelectShowText = "Choose seizure type";
+        internal const string MultiSelectHideText = "Hide seizure types";
+    }
+
+    internal static class SeizureTypes
+    {
+        internal const string Partial = "Partial";
+        internal const string Complex = "Complex";
+        internal const string Aura = "Aura";
+        internal const string GrandMal = "GrandMal";
+        internal const string Musicogenic = "Musicogenic";
+        internal const string SmellSensory = "SmellSensory";
+        internal const string HeatSensory = "HeatSensory";
+        internal const string Anxiety = "Anxiety";
+        internal const string MedicationChange = "MedicationChange";
+    }
+};
